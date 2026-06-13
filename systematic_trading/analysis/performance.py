0d38d8361169c6ffc6dc07098e09b3814a1da7cb
Jaeyoon Chung <jaeyoon.chung@student.manchester.ac.uk>
2026-06-13 17:57:01 +0900

    Added CAGR

--- systematic_trading/analysis/performance.py
+++ systematic_trading/analysis/performance.py
@@ -6,6 +6,8 @@ class PerformanceAnalyser:
         self.results = pd.DataFrame(results)
         self.returns = self.results["portfolio_value"].pct_change().dropna()
 
+    # the methods below return portfolio values from a selected strategy
+
     def total_return(self):
         start_value = self.results["portfolio_value"].iloc[0]
         end_value = self.results["portfolio_value"].iloc[-1]
@@ -19,24 +21,7 @@ class PerformanceAnalyser:
 
         return drawdown.min()
     
-    def summary(self):
-        return {
-            "total_return": round(float(self.total_return())*100, 2),
-            "max_drawdown": round(float(self.max_drawdown())*100, 2),
-            "sharpe_ratio": round(float(self.sharpe_ratio()), 2),
-            "buy_and_hold_return": round(float(self.buy_and_hold_return()) * 100, 2),
-            "excess_return": round(float((self.total_return() - self.buy_and_hold_return())) * 100, 2),
-            "buy_and_hold_max_drawdown": round(float(self.buy_and_hold_max_drawdown()) * 100, 2)
-        }
-    
-    def sharpe_ratio(self):
-        mean_return = self.returns.mean()
-        volatility = self.returns.std()
-
-        if volatility == 0:
-            return 0
-        
-        return (mean_return / volatility) * np.sqrt(252)
+    # Now we need the returns and drawdowns when we buy and hold the stocks
     
     def buy_and_hold_return(self):
         start_price = self.results["price"].iloc[0]
@@ -51,3 +36,38 @@ class PerformanceAnalyser:
 
         return drawdown.min()
     
+    # strategy alpha comparators
+    # 1. sharpe ratio
+    # 2. 
+    
+    def sharpe_ratio(self):
+        mean_return = self.returns.mean()
+        volatility = self.returns.std()
+
+        if volatility == 0:
+            return 0
+        
+        return (mean_return / volatility) * np.sqrt(252)
+    
+    def cagr(self):
+        start_value = self.results["portfolio_value"].iloc[0]
+        end_value = self.results["portfolio_value"].iloc[-1]
+
+        start_date = self.results["date"].iloc[0]
+        end_date = self.results["date"].iloc[-1]
+
+        total_days = (end_date - start_date).days
+        years = total_days / 365
+
+        return (end_value / start_value) ** (1 / years) - 1
+    
+    def summary(self):
+        return {
+            "total_return": round(float(self.total_return())*100, 2),
+            "max_drawdown": round(float(self.max_drawdown())*100, 2),
+            "sharpe_ratio": round(float(self.sharpe_ratio()), 2),
+            "cagr": round(float(self.cagr()) * 100, 2),
+            "buy_and_hold_return": round(float(self.buy_and_hold_return()) * 100, 2),
+            "excess_return": round(float((self.total_return() - self.buy_and_hold_return())) * 100, 2),
+            "buy_and_hold_max_drawdown": round(float(self.buy_and_hold_max_drawdown()) * 100, 2)
+        }
